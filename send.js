6c6ac0fcd869b8dda6e672be7f92701fd237ce7a
noemi <noemi@localhost.localdomain>
2017-08-23 19:43:51 +0200

    fix not showed hourse

---
+++ send.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#plot_submmit").click(function(){
+        value = document.getElementById("from_date").value;
+       value2 = document.getElementById("to_date").value;
+       console.log(value)
+       console.log(value2)
+    }); 
+});
